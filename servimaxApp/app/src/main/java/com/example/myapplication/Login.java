ca3c17fce2c46e821250aaf2fadd01e5739337fa
Oscar Garcia <oscare_gs@hotmail.com>
2022-04-19 23:39:21 -0500

    Conexion con autenticacion de usuarios de Firebase Ya se pueden registrar usuarios

--- servimaxApp/app/src/main/java/com/example/myapplication/Login.java
+++ servimaxApp/app/src/main/java/com/example/myapplication/Login.java
@@ -1,14 +1,80 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseAuth mAuth;
+
+    EditText emailET;
+    EditText passET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialize Firebase Auth
+//        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            Toast.makeText(this, "No usuario", Toast.LENGTH_SHORT).show();
+        }
+
+        emailET = findViewById(R.id.email_login_text);
+        passET = findViewById(R.id.password_text);
+    }
+
+    private void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("D", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("D", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user){
+
+    }
+
+    public void registro(View view) {
+        String email = emailET.getText().toString();
+        String pass = passET.getText().toString();
+
+        createAccount("oscare_gs@hotmail.com", "1234qwer");
     }
 }
--- servimaxApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ servimaxApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +31,26 @@ public class MainActivity extends AppCompatActivity {
         b.putString("message", "integracion completa");
         mFirebaseAnalytics.logEvent("InitScreep", b);
 
-        Intent intent = new Intent(this, ListaTrabajadores.class);
+
+//        // Initialize Firebase Auth
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser == null){
+//            Toast.makeText(this, "No usuario", Toast.LENGTH_SHORT).show();
+//        }
+
+        Intent intent = new Intent(this, Login.class);
         startActivity(intent);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
+
+
+
 }
