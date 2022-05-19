ecb87e47088470b7bd07b0262cb2337ad481490d
Oscar Garcia <oscare_gs@hotmail.com>
2022-05-18 21:04:11 -0500

    Implementacion de calificaciones

--- servimaxApp/app/src/main/java/com/example/myapplication/InfoTrabajador.java
+++ servimaxApp/app/src/main/java/com/example/myapplication/InfoTrabajador.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,8 +26,12 @@ public class InfoTrabajador extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth usr = FirebaseAuth.getInstance();
 
-    int califica;
+    int califica = 0;
     String trabID;
+    String ruta;
+    String usuario;
+
+    Button borrarCali;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +41,60 @@ public class InfoTrabajador extends AppCompatActivity {
         TextView nombreInfo = findViewById(R.id.nombreInfo);
         TextView calificiacionInfo = findViewById(R.id.calificacionInfo);
         ImageView fotoInfo = findViewById(R.id.fotoTrabajadorInfo);
+
+        Button b1 = findViewById(R.id.estrella1);
+        Button b2 = findViewById(R.id.estrella2);
+        Button b3 = findViewById(R.id.estrella3);
+        Button b4 = findViewById(R.id.estrella4);
+        Button b5 = findViewById(R.id.estrella5);
+
+        borrarCali = findViewById(R.id.borrarCali);
+        borrarCali.setVisibility(View.INVISIBLE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                califica = 1;
+                calificarTrabajador();
+                borrarCali.setVisibility(View.VISIBLE);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                califica = 2;
+                calificarTrabajador();
+                borrarCali.setVisibility(View.VISIBLE);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                califica = 3;
+                calificarTrabajador();
+                borrarCali.setVisibility(View.VISIBLE);
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                califica = 4;
+                calificarTrabajador();
+                borrarCali.setVisibility(View.VISIBLE);
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                califica = 5;
+                calificarTrabajador();
+                borrarCali.setVisibility(View.VISIBLE);
+            }
+        });
 
         trabID = getIntent().getStringExtra("trabajador");
 
@@ -53,7 +112,7 @@ public class InfoTrabajador extends AppCompatActivity {
 
         ///// PRUEBAAAa
 
-        calificarTrabajador();
+//        calificarTrabajador();
     }
 
 
@@ -66,15 +125,40 @@ public class InfoTrabajador extends AppCompatActivity {
         startActivity(intent);
     }
 
-    void calificarTrabajador(){
-        String usuario = usr.getCurrentUser().getEmail().toString();
 
-        String ruta = "users/" + trabID + "/calificaciones";
+    public void borrarCalificacion(View v){
+        db.collection(ruta).document(usuario)
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(InfoTrabajador.this, "Calififcacion borrada", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(InfoTrabajador.this, "La calificacion no pudo ser borrada", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        borrarCali.setVisibility(View.INVISIBLE);
+    }
+
+
+
+    void calificarTrabajador(){
+
+
+
+        usuario = usr.getCurrentUser().getEmail().toString();
+
+        ruta = "users/" + trabID + "/calificaciones";
 
 
         Map<String, Object> calificacion = new HashMap<>();
         calificacion.put("usuario", usuario);
-        calificacion.put("calificaion", califica);
+        calificacion.put("calificacion", califica);
 
 
 
@@ -86,7 +170,7 @@ public class InfoTrabajador extends AppCompatActivity {
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(InfoTrabajador.this, "No se pudo mandar la calificacion", Toast.LENGTH_SHORT).show();
+                Toast.makeText(InfoTrabajador.this, "No se pudo enviar la calificacion", Toast.LENGTH_SHORT).show();
             }
         });
 
