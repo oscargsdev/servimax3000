370db17442a4a3cf3db2848bd1c11f0370b01b92
male283 <19630283@itocotlan.com>
2022-05-19 17:37:24 -0500

    ..

--- servimaxApp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ servimaxApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registro);
 
         nombres = getResources().getStringArray(R.array.nombresRegistro);
         apellidos = getResources().getStringArray(R.array.apesRegistro);
